84c2be3a6eed1608305505e13158a20a1964bd14
Renato de O. Costa <renato_costa@yahoo.com.br>
2025-03-08 23:59:54 -0300

    Atualização de código com nova solução

--- Capitulo_4/exercicio_4.3.py
+++ Capitulo_4/exercicio_4.3.py
@@ -1,27 +1,34 @@
 # Exercício 4.3 - Verifica qual número é maior e qual o menor
 
 # Solicita três números para o usuário
-num_1 = int(input("Digite o primeiro número:\n"))
-num_2 = int(input("Digite o segundo número:\n"))
-num_3 = int(input("Digite o terceiro número:\n"))
+# num_1 = int(input("Digite o primeiro número:\n"))
+# num_2 = int(input("Digite o segundo número:\n"))   Obs. código da primeira solução
+# num_3 = int(input("Digite o terceiro número:\n"))
+a, b, c = input("Informe três números:\n").split()
+a = int(a)
+b = int(b)
+c = int(c)
 
-# Verificando o maior
-if num_1 > num_2 and num_1 > num_3:
+# Verificando o maior entre a e b
+'''if num_1 > num_2 and num_1 > num_3: 
     maior = num_1
-if num_2 > num_1 and num_2 > num_3:
+if num_2 > num_1 and num_2 > num_3: Obs. código da primeira solução
     maior = num_2
 if num_3 > num_1 and num_3 > num_2:
-    maior = num_3
+    maior = num_3'''
+maior_ab = (a + b + abs(a - b)) // 2
 
-# Verificando o menor
-if num_1 < num_2 and num_1 < num_3:
+
+# Verificando o maior entre maior_ab e c
+'''if num_1 < num_2 and num_1 < num_3:
     menor = num_1
-if num_2 < num_1 and num_2 < num_3:
+if num_2 < num_1 and num_2 < num_3: Obs. código da primeira solução
     menor = num_2
 if num_3 < num_1 and num_3 < num_2:
-    menor = num_3
+    menor = num_3'''
+maior = (maior_ab + c + abs(maior_ab - c)) // 2
 
 # Imprime resultado na tela
-print(f"O maior número é {maior} e o menor número é {menor}.")
+print(f"O maior número é {maior}.")
 
 
